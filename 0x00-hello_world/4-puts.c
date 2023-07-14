6a816afa5e861c5483bb4b0ee543a96edf2a230d
amMike001 <emperormike0001@gmail.com>
2023-07-14 13:32:18 -0700

    all done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main-is entry point of the program
+  *return-the program has no return value
+  **/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
